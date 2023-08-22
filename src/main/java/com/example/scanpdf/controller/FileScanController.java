1ec0135c5fa0aff0f450a9aa9326bcf0aeed2d29
Samuel Nganga <samuelnganga@Irembo-095-SNganga.local>
2023-08-22 18:48:57 +0200

    added the sonar.properties file

--- src/main/java/com/example/scanpdf/controller/FileScanController.java
+++ src/main/java/com/example/scanpdf/controller/FileScanController.java
@@ -6,6 +6,7 @@ import com.example.scanpdf.service.FileScanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
